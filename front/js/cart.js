1ec1f567aa41cf8aa6c03da501f9e45eb156a3ed
Jeffrey Blanc <blancjeffrey@gmail.com>
2021-11-21 23:09:23 +0100

    Creation fonctionnalite affichage des produits dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,39 @@
+function displayProductOrder(){
+  let itemProduct = document.getElementById('cart__items');
+
+  cart.map(productOrder => {
+   productOrder.product.map(productOrderChoiceColorOfQuantity =>{
+    itemProduct.innerHTML += generateProductOrder(productOrder, productOrderChoiceColorOfQuantity);
+   });
+  });
+}
+
+function generateProductOrder(productOrder, productOrderChoiceColorOfQuantity){
+  return (
+    `
+    <article class="cart__item" data-id="${productOrder.id}">
+                <div class="cart__item__img">
+                  <img src="${productOrder.imageUrl}" alt="${productOrder.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__titlePrice">
+                    <h2>${productOrder.name} ${productOrderChoiceColorOfQuantity.color}</h2>
+                    <p>${productOrder.price} €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : ${productOrderChoiceColorOfQuantity.quantity} </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productOrderChoiceColorOfQuantity.quantity}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+    `
+  )
+}
+
 storageAlreadyExist();
+displayProductOrder();

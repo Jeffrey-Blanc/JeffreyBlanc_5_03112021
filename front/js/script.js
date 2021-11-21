0f760d8424b7fcedee3885805172a1d07f93abb6
Jeffrey Blanc <blancjeffrey@gmail.com>
2021-11-21 20:49:34 +0100

    creation fichier storage.js

--- front/js/script.js
+++ front/js/script.js
@@ -115,27 +115,3 @@ let addToCart = document.getElementById('addToCart').addEventListener('click', (
   }
 });
 
-/***
- * Extrait les données localStorage s'il existe.
- */
-function storageAlreadyExist(){
-  if(localStorage.getItem('panier')){
-    return extractStorages();
-  }
-}
-
-/***
- * Extrait des données localStorage
- */
-function extractStorages() {
-  let storage = localStorage.getItem('panier');
-  let panier = JSON.parse(storage);
-  return cart = panier;
-}
-
-/***
- * Enregistre les données de l'objet cart dans localStorage.
- */
-function saveToStorage(){
-  localStorage.setItem('panier', JSON.stringify(cart));
-}
---
+++ front/js/storage.js
@@ -0,0 +1,24 @@
+/***
+ * Extrait les données localStorage s'il existe.
+ */
+ function storageAlreadyExist(){
+  if(localStorage.getItem('panier')){
+    return extractStorages();
+  }
+}
+
+/***
+ * Extrait des données localStorage
+ */
+function extractStorages() {
+  let storage = localStorage.getItem('panier');
+  let panier = JSON.parse(storage);
+  return cart = panier;
+}
+
+/***
+ * Enregistre les données de l'objet cart dans localStorage.
+ */
+function saveToStorage(){
+  localStorage.setItem('panier', JSON.stringify(cart));
+}
